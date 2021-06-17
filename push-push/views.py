39f4c50b5b4617277f51f05985264e53bb713390
Alex Troshkov <rudotcom@gmai.com>
2021-06-17 12:00:02 +0300

    start post

--- push-push/views.py
+++ push-push/views.py
@@ -1,8 +1,32 @@
-from django.views.decorators.http import require_GET
+from django.http.response import JsonResponse, HttpResponse
+from django.views.decorators.http import require_GET, require_POST
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 @require_GET
 def home(request):
     user = request.user
     return render(request, 'home.html')
+
+
+@require_POST
+@csrf_exempt
+def send_push(request):
+    try:
+        body = request.body
+        data = json.loads(body)
+
+        if 'head' not in data or 'body' not in data or 'id' not in data:
+            return JsonResponse(status=400, data={"message": "Invalid data format"})
+
+        user_id = data['id']
+        user = get_object_or_404(User, pk=user_id)
+        payload = {'head': data['head'], 'body': data['body']}
+        send_user_notification(user=user, payload=payload, ttl=1000)
+
+        return JsonResponse(status=200, data={"message": "Web push successful"})
+    except TypeError:
+        return JsonResponse(status=500, data={"message": "An error occurred"})
