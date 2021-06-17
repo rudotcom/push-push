22c87b43ccef1c8fde1dd2d74e9a3fedd3889b8f
go <rudotcom@gmail.com>
2021-06-17 13:58:05 +0300

    post failed

--- push-push/settings.py
+++ push-push/settings.py
@@ -128,3 +128,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_COOKIE_SECURE = True
--- push-push/views.py
+++ push-push/views.py
@@ -18,14 +18,11 @@ def send_push(request):
     try:
         body = request.body
         data = json.loads(body)
-
+        print(data)
         if 'head' not in data or 'body' not in data or 'id' not in data:
             return JsonResponse(status=400, data={"message": "Invalid data format"})
 
-        user_id = data['id']
-        user = get_object_or_404(User, pk=user_id)
         payload = {'head': data['head'], 'body': data['body']}
-        send_user_notification(user=user, payload=payload, ttl=1000)
 
         return JsonResponse(status=200, data={"message": "Web push successful"})
     except TypeError:
